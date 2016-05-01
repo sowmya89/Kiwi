5a77b7645174c4b7600e1adb139c38e2c7e5a64a
nishanth-prabhuswamy <nishanth.prabhu.68@gmail.com>
2016-05-01 13:56:05 -0700

    Updated wesberver to send gcm and store messages

--- Webserver/cryptoChat/webserver/urls.py
+++ Webserver/cryptoChat/webserver/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^send_message$', views.send_message, name='send_message'),
     url(r'^message$', views.process_message, name='process_message'),
+    url(r'^register_device$', views.register_device, name='register_device'),
+    url(r'^process_registeration$', views.process_registeration, name='process_registeration'),
 ]
--- Webserver/cryptoChat/webserver/views.py
+++ Webserver/cryptoChat/webserver/views.py
@@ -8,15 +8,22 @@ from django import forms
 from django.views.decorators.csrf import csrf_protect
 
 from kiwi import get_sentiment,evaluate_features
+from gcm import *
+import urllib3
+urllib3.disable_warnings()
 
 client = MongoClient('localhost', 27017)
 db = client['Kiwichat']
 send = True
+threshold_limit = 5
 
 # Create your views here.
 CSRF_COOKIE_SECURE = False
 def index(request):
     return render(request,'HTML/index.html',{})
+
+def register_device(request):
+    return render(request,'HTML/register_device.html',{})
 
 def process_message(request):
     cursor = db.chats.find()
@@ -29,6 +36,7 @@ def send_message(request):
     global send
     if request.method == 'POST':
         message = request.POST.get("message")
+        deviceId = request.POST.get("deviceId")
         print "message: ",message
         bar =evaluate_features(message)
         result = db.chats.insert_one(
@@ -37,7 +45,38 @@ def send_message(request):
             "polarity":bar
         }
         )
-        if(send == True):
+        cursor = db.threshold.find_one({'deviceId':deviceId })
+        threshold_value = cursor['threshold_value']
+        print "bar",bar
+        if(bar[0] == 'neg'):
+            threshold_value = threshold_value + 1
+            result = db.threshold.update_one(
+            {"deviceId": deviceId},
+            {
+                "$set": {
+                    "threshold_value": threshold_value
+                },
+            }
+            )
+        print "Threshold: ",threshold_value
+        if(threshold_value <= threshold_limit):
+                gcm = GCM("AIzaSyDL4b5qf3_iUGuqi__BGiQ2HKud5iA14rg")
+                data = {'the_message': message}
+                reg_id = 'APA91bFxz8_U87jN2fO1zRo-rXX2AjO8NBNqoLKunSyNYI2mZhdMkoXS9JWekDlrQk5cvi6T00QaFuhRHig8QFbhV49Kwk5GXu-MEtCFaj2ECq60D8hksJOc83pp99GwHdW3CGD$
+                gcm.plaintext_request(registration_id=reg_id, data=data)
                 print "forward"
 
         return HttpResponse(bar)
+
+@csrf_protect
+def process_registeration(request):
+    if request.method == 'POST':
+        deviceId = request.POST.get("deviceId")
+        print "deviceId: ",deviceId
+        result = db.threshold.insert_one(
+        {
+            "deviceId":deviceId,
+            "threshold_value":0
+        }
+        )
+        return HttpResponse("Success")
